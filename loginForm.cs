c02c93249ed1d436dee1347af30dbf28b752e668
blackjavk20 <duyquocdo19@gmail.com>
2023-12-18 14:21:11 +0700

    first demo

---
+++ loginForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VBStore
+{
+    public partial class loginForm : Form
+    {
+        public loginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void loginbtn_Click(object sender, EventArgs e)
+        {
+
+            string username = usernameTextBox.Text;
+            string password = passwordTextBox.Text;
+
+            if (username == "admin" && password == "admin")
+            {
+                mainForm mainForm = new mainForm();
+                mainForm.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Tên đăng nhập hoặc mật khẩu không chính xác", "Lỗi đăng nhập", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
